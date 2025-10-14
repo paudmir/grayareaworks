5c655fdc62aea23684e943111a50ae00b05c3c3d
Paulina Duran <winterdamsel@gmail.com>
2025-10-13 20:16:01 -0700

    typo fix

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("Hello World!");
 console.log("Just adding this to make sure it works.");
 
 let numClicks = 0; 
-function updateClickClount() {   
+function updateClickCount() {   
     numClicks = numClicks + 1;  
     console.log(numClicks); 
 }
