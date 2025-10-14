87639a7e977ebb4e0623db387a1ccb722307fe3e
Paulina Duran <winterdamsel@gmail.com>
2025-10-13 20:09:09 -0700

    Adding number of clicks button

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 console.log("Hello World!");
 console.log("Just adding this to make sure it works.");
+
+let numClicks = 0; 
+function updateClickClount() {   
+    numClicks = numClicks + 1;  
+    console.log(numClicks); 
+}
