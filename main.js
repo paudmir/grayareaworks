eea14f605565951e3fafbf320eb53c154cf8c7f4
Paulina Duran <winterdamsel@gmail.com>
2025-10-15 18:23:00 -0700

    adding a click function

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 console.log("Hello World!");
 console.log("Just adding this to make sure it works.");
 
+
 let numClicks = 0; 
 function updateClickCount() {   
     numClicks = numClicks + 1;  
     console.log(numClicks); 
+}
+
+document.body.addEventListener("click",logClickLocation);
+function logClickLocation(e){
+    console.log(e.clientX+" "+e.clientY);
 }
